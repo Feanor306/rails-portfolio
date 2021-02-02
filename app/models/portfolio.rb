928e688e9e264619716a539ca85e557b96437f24
Feanor306 <feanor306@live.com>
2021-02-02 19:24:51 +0100

    Defaults for Portfolio images

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -7,4 +7,13 @@ class Portfolio < ApplicationRecord
 	end
 
 	scope :ruby_on_rails_portfolio, -> { where(subtitle: "Ruby on rails")}
+
+	# Default value field initializer
+	after_initialize :set_defaults
+
+	def set_defaults
+		# ||= operator sets value only if the field is empty
+		self.main_image ||= "https://via.placeholder.com/600x400"
+		self.thumb_image ||= "https://via.placeholder.com/350x200"
+	end
 end
