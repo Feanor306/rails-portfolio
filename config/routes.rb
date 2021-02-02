2bc423c9c2ddb8fc2d8f8f850b9425c59eb4de51
Feanor306 <feanor306@live.com>
2021-02-02 11:17:40 +0100

    Custom route for portfolio show

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   get 'contact', to: 'pages#contact'	#get 'pages/contact'
 
   resources :categories
-  resources :portfolios
   resources :blogs
+
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
  
 end
