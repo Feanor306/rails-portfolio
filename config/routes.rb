bdda5d030731418d6317d73882b9a2cc85f6b831
Feanor306 <feanor306@live.com>
2021-02-02 13:27:55 +0100

    Custom toggle published action for blog

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,14 @@ Rails.application.routes.draw do
   get 'contact', to: 'pages#contact'	#get 'pages/contact'
 
   resources :categories
-  resources :blogs
 
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+
+  resources :blogs do 
+  	member do
+  	  get :toggle_status
+  	end
+  end
  
 end
