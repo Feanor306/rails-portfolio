10891377e20a19c853fb110a79d2e949f4dbf757
Feanor306 <feanor306@live.com>
2021-02-09 10:51:24 +0100

    Custom auth routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
+
   root to: 'pages#home' 				#get 'pages/home'
+
   get 'about', to: 'pages#about' 		#get 'pages/about'
   get 'contact', to: 'pages#contact'	#get 'pages/contact'
 
