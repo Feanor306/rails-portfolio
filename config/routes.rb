20d243623eb93fd460d47264faede689899aea4e
Feanor306 <feanor306@live.com>
2021-02-09 21:00:59 +0100

    Session tracking for dynamic source qp

--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,11 @@ Rails.application.routes.draw do
 
   get 'about', to: 'pages#about' 		#get 'pages/about'
   get 'contact', to: 'pages#contact'	#get 'pages/contact'
-
-  resources :categories
-
-  resources :portfolios, except: [:show]
   get 'angular-items', to: 'portfolios#angular'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+
+  resources :categories
+  resources :portfolios, except: [:show]
 
   resources :blogs do 
   	member do
