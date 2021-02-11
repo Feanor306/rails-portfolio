f3eb2984c5a635e850a82dfe9a2f69db58465fc0
Feanor306 <feanor306@live.com>
2021-02-11 15:35:18 +0100

    Before_action for portfolio controller

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,5 @@
 class PortfoliosController < ApplicationController
+	before_action :set_portfolio, only: [:edit, :update, :show, :destroy]
 	layout "portfolio"
 
 	def index
@@ -27,12 +28,9 @@ class PortfoliosController < ApplicationController
   	end
 
   	def edit
-  		@portfolio = Portfolio.find(params[:id])
   	end
 
   	def update
-	  	@portfolio = Portfolio.find(params[:id])
-
 	    respond_to do |format|
 	      if @portfolio.update(portfolio_params)
 	        format.html { redirect_to portfolios_path, notice: "Portfolio was successfully updated." }
@@ -43,12 +41,9 @@ class PortfoliosController < ApplicationController
 	end
     
     def show
-    	@portfolio = Portfolio.find(params[:id])
     end
 
     def destroy
-    	# Find item
-    	@portfolio = Portfolio.find(params[:id])
     	# Destroy item in DB
     	@portfolio.destroy
     	# Redirect
@@ -65,5 +60,9 @@ class PortfoliosController < ApplicationController
     		:subtitle,
     		:body, 
 	    	technologies_attributes: [:name])
+    end
+
+    def set_portfolio
+    	@portfolio = Portfolio.find(params[:id])
     end
 end
