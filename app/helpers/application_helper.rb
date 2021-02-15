b350e919a95f7fd7f52c7803c394ced807912d1c
Feanor306 <feanor306@live.com>
2021-02-15 14:42:38 +0100

    Navigation helper and active class

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,4 +21,61 @@ module ApplicationHelper
     DevcampViewToolFeanor::Renderer.copyright 'Feanor306', 'All rights reserved'
   end
 
+  def nav_items
+	[
+		{
+			url: root_path,
+			title: "Home"
+		},
+		{
+			url: about_path,
+			title: "About"
+		},
+		{
+			url: contact_path,
+			title: "Contact"
+		},
+		{
+			url: blogs_path,
+			title: "Blog"
+		},
+		{
+			url: portfolios_path,
+			title: "Portfolio"
+		},
+	]
+  end
+
+  def nav_helper style, wrap_tag
+	nav_links = ''
+
+	nav_items.each do |item|
+		nav_links << "<#{wrap_tag}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{wrap_tag}>"
+	end
+
+	nav_links.html_safe
+  end
+
+  def nav_helper_heredoc style, wrap_tag
+	# Heredoc syntax
+nav_links = <<NAV
+<#{wrap_tag}><a href="#{root_path}" class="#{style} #{active? root_path}">Home</a></#{wrap_tag}>
+<#{wrap_tag}><a href="#{about_path}" class="#{style} #{active? about_path}">About</a></#{wrap_tag}>
+<#{wrap_tag}><a href="#{contact_path}" class="#{style} #{active? contact_path}">Contact</a></#{wrap_tag}>
+<#{wrap_tag}><a href="#{blogs_path}" class="#{style} #{active? blogs_path}">Blog</a></#{wrap_tag}>
+<#{wrap_tag}><a href="#{portfolios_path}" class="#{style} #{active? portfolios_path}">Portfolio</a></#{wrap_tag}>
+NAV
+
+	nav_links.html_safe
+  end
+
+  def active? path
+	"active" if current_page? path
+  end
 end
+
+# <%= link_to "Home", root_path, class: 'nav-link' %>
+# <%= link_to "About", about_path, class: 'nav-link' %>
+# <%= link_to "Contact", contact_path, class: 'nav-link' %>
+# <%= link_to "Blog", blogs_path, class: 'nav-link' %>
+# <%= link_to "Portfolio", portfolios_path, class: 'nav-link' %>
