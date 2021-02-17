2adfb874e9c00759802122aa04ccbafd01237f1e
Feanor306 <feanor306@live.com>
2021-02-17 13:10:47 +0100

    Refactored alerts (still not working due to jquery)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -72,6 +72,18 @@ NAV
   def active? path
 	"active" if current_page? path
   end
+
+	def alerts
+		alert = (flash[:alert] || flash[:error] || flash[:notice])
+
+		if alert
+			alert_generator alert
+		end
+	end
+
+	def alert_generator msg
+		js add_gritter(msg, :title => "Attention!", sticky: false) 
+	end
 end
 
 # <%= link_to "Home", root_path, class: 'nav-link' %>
