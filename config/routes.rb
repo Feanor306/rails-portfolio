61c03dbb8be36aec39bdf405c047d8984584c9c8
Feanor306 <feanor306@live.com>
2021-02-17 14:34:40 +0100

    Tech news page - shows recent tweets about rails

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
 
   get 'about', to: 'pages#about' 		#get 'pages/about'
   get 'contact', to: 'pages#contact'	#get 'pages/contact'
+  get 'tech-news', to: 'pages#tech_news'
+
   get 'angular-items', to: 'portfolios#angular'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
